7bc7eda5d0c43f5a3621ef3cd8952d4c2bda7373
peter omunyidde <pomunyiddde@gmail.com>
2019-08-20 16:27:03 +0000

    Student course page

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -4,21 +4,23 @@ class Instructor::CoursesController < ApplicationController
 	def new
 	   @course = Course.new	
     end
+
     def create
     @course = current_user.courses.create(course_params)
     if @course.valid?
       redirect_to instructor_course_path(@course)
     else
       render :new, status: :unprocessable_entity	
-  end
+    end
 
-  def show
-    @course = Course.find(params[:id])
-  end
+    def show
+      @course = Course.find(params[:id])
+    end
 
-  private
+    private
 
-  def course_params
-    params.require(:course).permit(:title, :description, :cost)
-  end
+    def course_params
+      params.require(:course).permit(:title, :description, :cost)
+    end
 end
+
