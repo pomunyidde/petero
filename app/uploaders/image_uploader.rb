f25efa4d19126611757020a2ce829df5aea20058
peter omunyidde <pomunyiddde@gmail.com>
2019-08-23 19:43:43 +0000

    Resolution change

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,17 @@
+# encoding: utf-8
+
+class ImageUploader < CarrierWave::Uploader::Base
+
+  # Include RMagick or MiniMagick support:
+  # include CarrierWave::RMagick
+  include CarrierWave::MiniMagick
+
+  # Choose what kind of storage to use for this uploader:
+  #storage :file
+  storage :fog
+
+  process resize_to_fill: [800, 350]
+
+
+  # A bunch more comments down here....
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_21_153028) do
+ActiveRecord::Schema.define(version: 2019_08_23_140805) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2019_08_21_153028) do
     t.integer "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
     t.index ["user_id"], name: "index_courses_on_user_id"
   end
 
@@ -32,6 +33,14 @@ ActiveRecord::Schema.define(version: 2019_08_21_153028) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["section_id"], name: "index_lessons_on_section_id"
+  end
+
+  create_table "photos", force: :cascade do |t|
+    t.text "caption"
+    t.integer "place_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "sections", force: :cascade do |t|
