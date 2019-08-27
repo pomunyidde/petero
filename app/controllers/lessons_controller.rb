1e93e5b37993224948d98fe1206480efa45b3a23
peter omunyidde <pomunyiddde@gmail.com>
2019-08-27 17:22:52 +0000

    Payment with Stripe

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,27 @@
 class LessonsController < ApplicationController
+   before_action :authenticate_user!
+   before_action :require_authorized_for_current_course, only: [:show]
+
+   def new
+     @lesson = Lesson.new
+   end	
+
+   def create
+   	@lesson = current_lesson.section.course(lesson_params)
+   	if current_user.enrolled_in?
+   		redirect_to course_path(@course)
+   end	
+
+
    def show
    end	
    
    private
+
+   def require_authorized_for_current_lesson
+   if current_lesson.user != current_user
+      redirect_to course_path, alert: 'You must be enrolled in'	
+    end  
 
   helper_method :current_lesson
   def current_lesson
